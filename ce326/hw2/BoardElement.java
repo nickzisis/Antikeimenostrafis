84cbd9bb1651efa6c9f4d25f664544c0615ed395
adcn123 <adcn551@gmail.com>
2026-03-27 11:48:03 +0200

    board element

--- ce326/hw2/BoardElement.java
+++ ce326/hw2/BoardElement.java
@@ -1,6 +1,16 @@
 package ce326.hw2;
 
-public class BoardElement extends Object{
+public abstract class BoardElement {
     private int[] position = new int[2];
+    private String symbol;
 
+    public BoardElement(int row, int column, String symbol) {
+        this.position[0] = row;
+        this.position[1] = column;
+        this.symbol= symbol;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
 }   
